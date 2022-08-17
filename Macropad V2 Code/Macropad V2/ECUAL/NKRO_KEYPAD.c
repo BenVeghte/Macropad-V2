0598bdee953dc6ec70424af36e6ad822e521a375
BenVeghte <Benveghte@live.com>
2022-08-17 09:24:14 -0400

    Create NKRO_KEYPAD.c

---
+++ Macropad V2 Code/Macropad V2/ECUAL/NKRO_KEYPAD.c
@@ -0,0 +1,11 @@
+/*
+ * NKRO_KEYPAD.c
+ *
+ *  Created on: Aug 15, 2022
+ *      Author: Ben Veghte
+ */
+
+#include <NKRO_KEYPAD.h>
+
+/* NKRO Keypad Configuration Struct */
+NKRO_KEYPAD_CFG Keypad
