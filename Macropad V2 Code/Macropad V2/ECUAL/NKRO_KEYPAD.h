d2a9f7206ae419e5b6d96115567ac0f9acd21e85
BenVeghte <Benveghte@live.com>
2022-08-17 09:24:30 -0400

    Update NKRO_KEYPAD.h
    Reformat comment structure

--- Macropad V2 Code/Macropad V2/ECUAL/NKRO_KEYPAD.h
+++ Macropad V2 Code/Macropad V2/ECUAL/NKRO_KEYPAD.h
@@ -13,18 +13,19 @@
 
 #include <stm32f0xx_hal.h>
 
-//Define Pressed and Released Values
+/*Define Pressed and Released Values*/
 #define KEY_PRESSED 1
 #define KEY_RELEASED 0
 
-//Define Number of Rows and Columns
+/*Define Number of Rows and Columns*/
 #define KEYPAD_ROWS 6
 #define KEYPAD_COLS 5
 
-//Define Scan Direction
+/*Define Scan Direction*/
 //if the diodes are connected to the rows, change the following line to #define ROW_SCAN
 #define COL_SCAN
 
+/*Keypad Configuration struct*/
 typedef struct
 {
 	uint16_t ROW_PIN[KEYPAD_ROWS];
@@ -34,4 +35,5 @@ typedef struct
 void NKRO_KEYPAD_Init(uint16_t au16_Instance, unint8_t* au8_KeyStates);
 void NKRO_KEYPAD_Scane(uint16_t au16_Instance, unint8_t* au8_KeyStates);
 
-#endif /*NKRO_KEYPAD_H_
+#endif /*NKRO_KEYPAD_H_*/
+
