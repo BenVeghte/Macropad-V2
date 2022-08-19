b7bb5c0616df890fd75fd2494bc83bba99221516
BenVeghte <Benveghte@live.com>
2022-08-18 20:42:04 -0400

    Create NRKOKeypadPressReleaseCheck

--- Macropad V2 Code/Macropad V2/ECUAL/NKRO_KEYPAD.c
+++ Macropad V2 Code/Macropad V2/ECUAL/NKRO_KEYPAD.c
@@ -16,56 +16,47 @@ static KeypadInfoTypeDef KeypadInfo;
 /*Function for the initialization of the keypad pins*/
 //This function needs to get called after the rest of the pin initialization is called to prevent
 // the settings being overridden
-void NKROKeypadInit(bool *b_key_states, uint32_t *u32_key_history)
-{
+void NKROKeypadInit(uint8_t *b_key_states, uint32_t *u32_key_history) {
     uint8_t i = 0;
     GPIO_InitTypeDef GPIO_InitStruct = {0};
     /*Make sure memory addresses to key_states and key_history are properly supplied*/
-    if(u16_key_states != NULL)
-    {
+    if(u16_key_states != NULL) {
         KeypadInfo.key_states = b_key_states;
     }
-    else
-    {
+    else {
 
         return;
     }
-    if(u32_key_history != NULL)
-    {
+    if(u32_key_history != NULL) {
         KeypadInfo.key_history = u32_key_history;
     }
-    else
-    {
+    else {
         //Should this throw an error? not entirely sure what this means
         return;
     }
 
     /*Defining pin modes*/
     #ifdef COL_SCAN
-    for(i=0; i<KEYPAD_ROWS; i++)
-    {
+    for(i=0; i<KEYPAD_ROWS; i++) {
         GPIO_InitStruct.Pin = KeypadCfgParams.row_pin[i];
         GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
         HAL_GPIO_Init(KeypadCfgParams.row_pin[i], &GPIO_InitStruct);
     }
-    for(i=0; i<KEYPAD_COLS; i++)
-    {
+    for(i=0; i<KEYPAD_COLS; i++) {
         GPIO_InitStruct.Pin = KeypadCfgParams.col_pin[i];
         GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
         GPIO_InitStruct.Pull = GPIO_PULLDOWN;
         HAL_GPIO_Init(KeypadCfgParams.col_pin[i], &GPIO_InitStruct);
     }
     #elifdef ROW_SCAN
-    for(i=0; i<KEYPAD_COLS; i++)
-    {
+    for(i=0; i<KEYPAD_COLS; i++) {
         GPIO_InitStruct.Pin = KeypadCfgParams.col_pin[i];
         GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
         HAL_GPIO_Init(KeypadCfgParams.col_pin[i], &GPIO_InitStruct);
     }
-    for(i=0; i<KEYPAD_ROWS; i++)
-    {
+    for(i=0; i<KEYPAD_ROWS; i++) {
         GPIO_InitStruct.Pin = KeypadCfgParams.row_pin[i];
         GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
         GPIO_InitStruct.Pull = GPIO_PULLDOWN;
@@ -84,61 +75,51 @@ void NKROKeypadScan() {
 
     /*Check to see if any key is pressed to save calculations*/
 #ifdef COL_SCAN
-    for(i=0;i<KEYPAD_ROWS; i++)
-    {
+    for(i=0;i<KEYPAD_ROWS; i++) {
         HAL_GPIO_WritePin(KeypadCfgParams.row_gpio_family[i], KeypadCfgParams.row_pin[i], 1);
     }
-    for(i=0;i<KEYPAD_COLS;i++)
-    {
+    for(i=0;i<KEYPAD_COLS;i++) {
         states = HAL_GPIO_ReadPin(KeypadCfgParams.col_gpio_family[i], KeypadCfgParams.col_pin[i]);
     }
-    for(i=0;i<KEYPAD_ROWS; i++)
-    {
+    for(i=0;i<KEYPAD_ROWS; i++) {
         HAL_GPIO_WritePin(KeypadCfgParams.row_gpio_family[i], KeypadCfgParams.row_pin[i], 0);
     }
 #elifdef ROW_SCAN
-    for(i=0;i<KEYPAD_COLS; i++)
-    {
+    for(i=0;i<KEYPAD_COLS; i++) {
         HAL_GPIO_WritePin(KeypadCfgParams.col_gpio_family[i], KeypadCfgParams.col_pin[i], 1);
     }
-    for(i=0;i<KEYPAD_ROWS;i++)
-    {
+    for(i=0;i<KEYPAD_ROWS;i++) {
         key_pressed = keypressed + HAL_GPIO_ReadPin(KeypadCfgParams.row_gpio_family[i], KeypadCfgParams.row_pin[i]);
     }
-    for(i=0;i<KEYPAD_COLS; i++)
-    {
+    for(i=0;i<KEYPAD_COLS; i++) {
         HAL_GPIO_WritePin(KeypadCfgParams.col_gpio_family[i], KeypadCfgParams.col_pin[i], 0);
     }
 
 #endif
     /*If any key is pressed*/
-    if(key_pressed>=1)
-    {
+    if(key_pressed>=1) {
         #ifdef COL_SCAN
-        for(i=0;i<KEYPAD_ROWS; i++)
-        {
+        for(i=0;i<KEYPAD_ROWS; i++) {
             HAL_GPIO_WritePin(KeypadCfgParams.row_gpio_family[i], KeypadCfgParams.row_pin[i], 1);
-            for(j=0;j<KEYPAD_COLS;j++)
-            {
+
+            for(j=0;j<KEYPAD_COLS;j++) {
                 KeypadInfo.key_history[i][j] = KeypadInfo.key_history[i][j] << 1;
-                if (HAL_GPIO_ReadPin(KeypadCfgParams.col_gpio_family[j], KeypadCfgParams.col_pin[j]))
-                {
+
+                if (HAL_GPIO_ReadPin(KeypadCfgParams.col_gpio_family[j], KeypadCfgParams.col_pin[j])) {
                     KeypadInfo.key_history[i][j]++;
                 }
                 // NEED Someway to track button press, and whether the button was just pressed or released, long press info necessary
 
             }
             HAL_GPIO_WritePin(KeypadCfgParams.row_gpio_family[i], KeypadCfgParams.row_pin[i], 0);
+
         }
         #elifdef ROW_SCAN
-        for(j=0;j<KEYPAD_COLS; j++)
-        {
+        for(j=0;j<KEYPAD_COLS; j++) {
             HAL_GPIO_WritePin(KeypadCfgParams.col_gpio_family[j], KeypadCfgParams.col_pin[j], 1);
-            for(i=0;i<KEYPAD_ROWS;i++)
-            {
+            for(i=0;i<KEYPAD_ROWS;i++) {
                 KeypadInfo.key_history[i][j] = KeypadInfo.key_history[i][j] << 1;
-                if (HAL_GPIO_ReadPin(KeypadCfgParams.row_gpio_family[i], KeypadCfgParams.row_pin[i]))
-                {
+                if (HAL_GPIO_ReadPin(KeypadCfgParams.row_gpio_family[i], KeypadCfgParams.row_pin[i])) {
                     KeypadInfo.key_history[i][j]++;
                 }
                 // NEED Someway to track button press, and whether the button was just pressed or released, long press info necessary
@@ -146,10 +127,47 @@ void NKROKeypadScan() {
             }
             HAL_GPIO_WritePin(KeypadCfgParams.col_gpio_family[j], KeypadCfgParams.col_pin[j], 0);
         }
+        #endif
 
     }
+}
+
+void NKROKeypadPressReleaseCheck() {
+    uint8_t i = 0;
+    uint8_t j = 0;
+    uint8_t states_changed = 0;
+
+    for(i=0;i<KEYPAD_ROWS*KEYPAD_COLS;i++) {
+        // Fill keypad key updates rows with an impossible value
+        // this will allow for a single comparison to be made to check if the value is one that has recently changed
+        KeypadInfo.key_updates[i].row = KEYPAD_ROWS;
+    }
+    KeypadInfo.key_updates = {0}; // this probably won't work
 
 
 
+    for(i=0;i<KEYPAD_ROWS;i++) {
+        for(j=0;j<KEYPAD_COLS;j++) {
+            //Key passes debouncing in the high state (button pressed), and the last value in the key state is low
+            if((KeypadInfo.key_history[i][j] & DEBOUNCE_MASK) == DEBOUNCE_MASK, (~KeypadInfo.key_states[i][j] & 1)==1) {
+                KeypadInfo.key_states[i][j] = KeypadInfo.key_states[i][j] << 1;
+                KeypadInfo.key_states[i][j]++;
 
+                KeypadInfo.key_updates[states_changed].row = i;
+                KeypadInfo.key_updates[states_changed].col = j;
+                KeypadInfo.key_updates[states_changed].state = 1;
+                states_changed++;
+            }
+
+            //Key passes debouncing in the low state (button released), and the last value in the key state is high
+            else if ((~KeypadInfo.key_history[i][j] & DEBOUNCE_MASK) == DEBOUNCE_MASK, (KeypadInfo.key_states[i][j] & 1)==1) {
+                KeypadInfo.key_states[i][j] = KeypadInfo.key_states[i][j] << 1;
+
+                KeypadInfo.key_updates[states_changed].row = i;
+                KeypadInfo.key_updates[states_changed].col = j;
+                KeypadInfo.key_updates[states_changed].state = 0;
+                states_changed++;
+            }
+        }
+    }
 }
--- Macropad V2 Code/Macropad V2/ECUAL/NKRO_KEYPAD.h
+++ Macropad V2 Code/Macropad V2/ECUAL/NKRO_KEYPAD.h
@@ -27,23 +27,32 @@
 #define COL_SCAN
 
 /*Keypad Configuration struct*/
-typedef struct NKROKeypadCFGTypeDef{
+typedef struct NKROKeypadCFGTypeDef {
     uint16_t row_pin[KEYPAD_ROWS];
     GPIO_TypeDef * row_GPIO_family[KEYPAD_ROWS];
     uint16_t col_pin[KEYPAD_COLS];
     GPIO_TypeDef * col_GPIO_family[KEYPAD_COLS]
 };
-typedef struct KeypadInfoTypeDef
-{
-    bool *key_states;
-    uint32_t *key_history;
+
+typedef struct KeyStateChangeTypeDef {
+    uint8_t row;
+    uint8_t col;
+    uint8_t state;
+};
+
+typedef struct KeypadInfoTypeDef {
+    uint8_t *key_states[KEYPAD_ROWS][KEYPAD_COLS];
+    uint32_t *key_history[KEYPAD_ROWS][KEYPAD_COLS];
+    KeyStateChangeTypeDef *key_updates[KEYPAD_COLS*KEYPAD_ROWS];
 };
 
 
 
+
+
 void NKROKeypadInit(unint16_t *au16_key_states);
-int DebounceMaskCalculator(uint32_t bit_length);
 void NKROKeypadScan();
+void NKROKeypadPressReleaseCheck();
 
 #endif /*NKRO_KEYPAD_H_*/
 
