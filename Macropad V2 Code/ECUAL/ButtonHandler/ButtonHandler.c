5e8d62b9a954750f8d9c903d3d49ea2a25fc5054
BenVeghte <Benveghte@live.com>
2022-12-20 22:14:24 -0800

    Delete ButtonHandler

--- Macropad V2 Code/ECUAL/ButtonHandler/ButtonHandler.c
+++
@@ -1,7 +0,0 @@
-/*
- * ButtonHandler.c
- *
- *  Created on: 8/20/2022
- *      Author: Ben Veghte
-*/
-#include <ButtonHandler.h>
--- Macropad V2 Code/ECUAL/ButtonHandler/KeyFunctions.c
+++
@@ -1,7 +0,0 @@
-/*
- * KeyFunctions.c
- *
- *  Created on: 8/20/2022
- *      Author: Ben Veghte
-*/
-#include <KeyFunctions.h>
